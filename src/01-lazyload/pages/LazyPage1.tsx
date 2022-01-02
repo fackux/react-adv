a8033c35e6508f7bf2278b3c8071963b2f532d85
fackux <flopez@navent.com>
2022-01-02 02:51:20 -0300

    listo clase 36

---
+++ src/01-lazyload/pages/LazyPage1.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const LazyPage1 = () => {
+    return (
+        <h1>Lazy 01</h1>
+    )
+}
+
+export default LazyPage1
---
+++ src/01-lazyload/pages/LazyPage2.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const LazyPage2 = () => {
+    return (
+        <h1>Lazy 02</h1>
+    )
+}
+
+export default LazyPage2
